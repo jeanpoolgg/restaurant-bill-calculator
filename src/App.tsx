c323ae23624c42c981f0575ada1b2be7576b1f08
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-09 23:34:27 -0500

    feat(layout): add productGrid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Calculator, Moon, Sun } from "lucide-react";
 import { Cart } from "./components/Cart";
+import { ProductGrid } from "./components/ProductGrid";
 
 function App() {
   return (
@@ -24,7 +25,10 @@ function App() {
         {/* Main Content */}
         <main className="mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-            <div className="lg:col-span-2">Product Section</div>
+            <div className="lg:col-span-2">
+              <ProductGrid />
+            </div>
+
             <Cart />
           </div>
         </main>
---
+++ src/components/ProductGrid.tsx
@@ -0,0 +1,8 @@
+
+export const ProductGrid = () => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 bg-white rounded-2xl p-8 text-center border border-gray-100">
+        <div className="w-full h-72 bg-white rounded-2xl"></div>
+    </div>
+  )
+}
